231120f54a7344100a7ecd7effe3ea1ebf59d785
Nukella Shalini <nukella_shalini@epam.com>
2025-10-27 00:40:49 +0530

    modified comments, updated readme, improved modularity

--- app/cache.py
+++ app/cache.py
@@ -1,22 +1,3 @@
-# import redis
-
-# cache = redis.Redis(
-#     host="localhost",   # Redis runs locally via Docker port mapping
-#     port=6379,
-#     db=0,
-#     decode_responses=True,
-# )
-
-# # expiry in - 5 mins
-# def cache_set(key, value, ttl=300):
-#     cache.setex(key, ttl, value)
-
-# def cache_get(key):
-#     return cache.get(key)
-
-# def cache_delete(key):
-#     cache.delete(key)
-
 import redis
 
 # Redis client
--- app/observer/base.py
+++ app/observer/base.py
@@ -1,5 +1,5 @@
 class Observer:
-    """Base class for all observers."""
+    #Base class for all observers.
 
     def update(self, media, review, session):
         """Handle an update event when a review is added."""
